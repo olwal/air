50b91f90da05d7a92721fbd86ae3694310db629b
Alex Olwal <alex@harmonic.se>
2020-12-31 00:09:14 -0800

    Updated with labels for Bay Area cities

--- js/features.js
+++ js/features.js
@@ -7,15 +7,56 @@
 
 class Features
 {
+  static cities;
+
+  static preload()
+  {
+    console.log(LANDMARKS_PATH);
+    Features.cities = loadTable(LANDMARKS_PATH, 'csv', 'header');
+  }
+
   static buildFromData(data, name)
   {
     let o = {};
     o.type = "FeatureCollection";
     o.name = name;
     o.features = [];
-    
+/*    o.defaults = {
+      "properties": {
+        "borderRadius": 25,
+        "padding": 10,
+        "borderWidth": 0,
+        },        
+      }
+    };    
+ */   
+
     for (let i = 0; i < data.length; i++)
     {
+  /*    let f =  {
+        "geometry": {
+          "type": "Point",
+          "coordinates": [data[i][1], data[i][2]]
+        },
+        "type": "Feature",
+        "id": 0,
+        "properties": {
+          "color": "#ddd",
+          "fontSize": 400,
+          "name": "|",
+          "anchorOffset": {
+            "y": -70,
+            "x": 0
+          },
+          "collapseDistance": 3000,
+          "fadeDistance": 5000,          
+          "anchor": "bottom"
+        }
+      };
+  
+      o.features.push(f);
+*/
+
       let node = {};
       node.type = 'Feature';
       node.id = data[i][0];
@@ -31,19 +72,180 @@ class Features
         continue;
 
       let colorAqi = data[i][8];
+      //'#000000'; //data[i][8];
 
       node.properties = {};
-      node.properties.borderRadius = 25;
+//      node.properties.fontSize = 40;
+      node.properties.borderRadius = 0;
       node.properties.color = '#ffffff';
-      node.properties.padding = 10;
+      node.properties.padding = 0;
+  //    node.properties.name = data[i][8];
       //node.properties.name = data[i][7];//aqi;
-      node.properties.background = colorAqi; //data[i][4];
+  //    node.properties.background = colorAqi; // + "33"; //data[i][4];
 
+//      o.features.push(node);
+
+//      node.id = "#" + data[i][0] + " bar";
+      node.properties.color = colorAqi;
+      node.properties.fontSize = 20 + 150 * min(5000, Math.pow(aqi, 1.5))/5000;
+      
+ //Math.max(20 + 2*aqi;
+      node.properties.name = "|";
+      node.properties.anchor = "bottom";
+      node.properties.fadeDistance = 100000;
+      //node.properties.anchorOffset = { x: 0, y: -(20 + 2*aqi)/5 };
+      node.properties.anchorOffset = { x: 0, y: 0 };
       o.features.push(node);
+/*
+      let base = {};
+      base.type = 'Feature';
+
+      let geometry2 = {};
+      geometry2.type = 'Point';
+      geometry2.coordinates = [data[i][1], data[i][2]];
+      base.geometry = geometry2;
+      base.id = "#" + data[i][0] + " base";
+      base.properties = {};
+      base.properties.fontSize = 10;
+      base.properties.color = colorAqi;
+      base.properties.name = "";
+      base.properties.anchor = "center";
+      base.properties.anchorOffset = {};
+      base.properties.icon = "circle-o";
+      o.features.push(base);      
+*/
     }
     
     console.log("Read " + data.length + " features.");
 
     return o;
   }
-};
+
+  static getBayAreaFeatures()
+  {   
+    let o = {};
+    o.type = "FeatureCollection";
+    o.name = "cities";
+    /*o.defaults = {
+      "properties": {
+        "color": "white",
+        "padding": 10,
+        "clipping": "object",
+        "borderRadius": 25,
+        "padding": 10,
+        "borderWidth": 0,
+        "background": "rgba(255, 255, 255, 200)",
+        "anchorOffset": {
+          "y": 70,
+          "x": 0
+        },        
+      }*/
+   // };
+
+
+    let count = 0;
+
+    o.features = [];
+
+    console.log("length: " + Features.cities.rows.length);    
+
+    for (let row of Features.cities.rows)
+    {
+/*      count += 1;
+      if (count > 10)
+        break;
+*/
+      if (row.arr[3] == '0')
+        continue;
+
+      let name = row.arr[0];
+      let latitude = parseFloat(row.arr[1]);
+      let longitude = parseFloat(row.arr[2]);
+      let show = row.arr[3];
+
+      let feature = {
+        "geometry": {
+          "type": "Point",
+          "coordinates": [longitude, latitude]
+        },
+        "type": "Feature",
+        "id": count,
+        "properties": {
+          "fontSize": 10 + (50 / (show * show)),
+          "color": "white",        
+          "name": name,
+          "anchorOffset": {
+            "y": 0,
+            "x": 0
+          },
+          "anchor": "bottom",
+          "borderRadius": 25,
+          "padding": 10,
+          "borderWidth": 0,
+//          "background": "rgba(255, 255, 255, 0.5)"
+          "fadeDistance": 250000,
+          }
+        };
+
+      let feature2 =
+        {
+          "geometry": {
+            "type": "Point",
+            "coordinates": [ -122.0839, 37.3861 ]
+          },
+          "type": "Feature",
+          "id": count,
+          "properties": {
+            "fontSize": 24,
+            "color": "white",        
+            "name": "Mountain View",
+          }
+        }
+
+      count += 1;
+      o.features.push(feature);
+    } 
+
+      
+
+
+
+/*
+      node.id = "#" + row.arr[0] + " bar";
+      node.properties.fontSize = 2;
+      node.properties.name = "|";
+
+      o.features.push(node);      
+   */   
+    //}
+
+    /*
+    let feature = 
+    {
+    "type": "FeatureCollection",
+    "features": [ {
+      "geometry": {
+        "type": "Point",
+        "coordinates": [ -122.0839, 37.3861 ]
+      },
+      "type": "Feature",
+      "id": 0,
+      "properties": {
+        "fontSize": 24,
+        "color": "white",        
+        "name": "Mountain View",
+        "anchorOffset": {
+          "y": 70,
+          "x": 0
+        },
+        "borderRadius": 25,
+        "padding": 10,
+        "borderWidth": 0,
+        "background": "rgba(33, 00, 00, 200)"
+        }
+      } ]
+    };*/
+
+    return o;
+  }
+}
